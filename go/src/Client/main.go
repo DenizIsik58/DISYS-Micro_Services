705d437d3b98fdae7f3ec7859209b43016769c6c
Deniz Yildirim <denizyil@outlook.dk>
2021-10-04 19:32:45 +0200

    Clean up client code

--- go/src/Client/main.go
+++ go/src/Client/main.go
@@ -8,75 +8,64 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"strconv"
 )
+
+const URL = "http://localhost:7000"
 
 func main() {
 
 	fmt.Println("Client successfully started....")
-	fmt.Println("Please simply pick one of these 5 options")
-	fmt.Println()
-	fmt.Println("Press 1 to add a new course")
-	fmt.Println("Press 2 to delete an existing course")
-	fmt.Println("Press 3 to delete a student or a teacher from a course")
-	fmt.Println("Press 4 to change the name of an existing course")
-	fmt.Println("Press 5 to get an overview of current courses")
-	var k string
-	fmt.Scanln(&k)
 
-	switch k {
-	case "1":
-		fmt.Println("To add a course, simply write a name in the terminal and hit enter")
+	for {
 
-		var courseName string
-		fmt.Scanln(&courseName)
-		addCourse(courseName)
-		break
-	case "2":
-		fmt.Println("To delete an existing course, simply write the id in the terminal and hit enter")
+		fmt.Println("Please simply pick one of these 5 options")
+		fmt.Println()
+		fmt.Println("Press 1 to add a new course")
+		fmt.Println("Press 2 to delete an existing course")
+		fmt.Println("Press 3 to delete a student or a teacher from a course")
+		fmt.Println("Press 4 to change the name of an existing course")
+		fmt.Println("Press 5 to get an overview of current courses")
+		var selection string
+		fmt.Scanln(&selection)
 
-		var courseId int
-		fmt.Scanln(&courseId)
-		deleteCourse(courseId)
-		break
-	case "3":
-		fmt.Println("To delete a teacher or a student from a course, please type TEACHER or STUDENT to delete either of these followed by the courseid and student/teacherid, id int, courseId int")
+		switch selection {
+		case "1":
+			fmt.Println("To add a course, simply write a name in the terminal and hit enter")
 
-		var searchType, typeId, courseid string
-		fmt.Scan(&searchType)
-		fmt.Scan(&typeId)
-		fmt.Scan(&courseid)
-		break
+			var courseName string
+			fmt.Scanln(&courseName)
+			addCourse(courseName)
+		case "2":
+			fmt.Println("To delete an existing course, simply write the id in the terminal and hit enter")
 
-	case "4":
-		fmt.Println("To change the name of an existing course, please type a name in the terminal followed by the id of the course")
+			var courseId string
+			fmt.Scanln(&courseId)
+			deleteCourse(courseId)
+		case "3":
+			fmt.Println("To delete a teacher or a student from a course, please type TEACHER or STUDENT to delete either of these followed by the courseid and student/teacherid, id int, courseId int")
 
-		var courseName string
-		var courseId int
-		fmt.Scan(&courseName)
-		fmt.Scan(&courseId)
-		changeNameOfCourse(courseName, courseId)
+			var searchType, typeId, courseid string
+			fmt.Scan(&searchType)
+			fmt.Scan(&typeId)
+			fmt.Scan(&courseid)
+		case "4":
+			fmt.Println("To change the name of an existing course, please type a name in the terminal followed by the id of the course")
 
-		break
-
-	case "5":
-		fmt.Println("You requested to get an overview of all existing courses! \n")
-		getAllTeachers()
-		break
+			var courseName string
+			var courseId string
+			fmt.Scan(&courseName)
+			fmt.Scan(&courseId)
+			changeNameOfCourse(courseName, courseId)
+		case "5":
+			fmt.Println("You requested to get an overview of all existing courses!")
+			fmt.Println(" ")
+			getAllTeachers()
+		}
 	}
-
-
-
-
-
-
-
 }
 
-
 func addCourse(input string) {
-
-	_, err := http.PostForm("http://localhost:7000/courses", url.Values{"name": {input}})
+	_, err := http.PostForm(URL+"/courses", url.Values{"name": {input}})
 
 	if err != nil {
 		log.Fatal(err)
@@ -84,44 +73,35 @@ func addCourse(input string) {
 
 }
 
-func deleteCourse(courseID int) {
-	_, err := http.NewRequest("DELETE", "http://localhost:7000/courses/1", nil)
+func deleteCourse(courseID string) {
+	_, err := http.NewRequest("DELETE", URL+"/courses/"+courseID, nil)
 
 	if err != nil {
-		fmt.Println("Error occured")
+		log.Fatalln(err)
 	}
 
 }
 
-func deleteTeacherStudent(enrollmentType string, id int, courseId int){
-
-	courseIdToString := strconv.Itoa(courseId)
-	IdToString := strconv.Itoa(id)
-
-	_, err := http.NewRequest("DELETE", "http://localhost:7000/courses" + courseIdToString + "/" + IdToString + "/" + enrollmentType,nil)
+func deleteTeacherStudent(enrollmentType string, id string, courseId string) {
+	_, err := http.NewRequest("DELETE", URL+"/courses"+courseId+"/"+id+"/"+enrollmentType, nil)
 
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln(err)
 	}
 }
 
-func changeNameOfCourse(newCourseName string, courseId int) {
-
-	courseIdToString := strconv.Itoa(courseId)
-
+func changeNameOfCourse(newCourseName string, courseId string) {
 	jsonStr := "{title:" + newCourseName + "}"
 
-
-	_, err := http.NewRequest("PUT","http://localhost:7000/courses" + courseIdToString, bytes.NewReader([]byte(jsonStr)) )
+	_, err := http.NewRequest("PUT", URL+"/courses"+courseId, bytes.NewReader([]byte(jsonStr)))
 
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln(err)
 	}
-
 }
 
-func getAllTeachers(){
-	resp, err := http.Get("http://localhost:7000/courses")
+func getAllTeachers() {
+	resp, err := http.Get(URL + "/courses")
 
 	if err != nil {
 		log.Fatalln(err)
@@ -140,7 +120,5 @@ func getAllTeachers(){
 	}
 
 	sb := string(body)
-	log.Printf(sb)
+	log.Println(sb)
 }
-
-
