79bdbbe718c2bc97adc5c92c3392816fdf205f45
Deniz Yildirim <denizyil@outlook.dk>
2021-10-05 00:28:38 +0200

    Fix all requests

--- go/src/Client/main.go
+++ go/src/Client/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -13,12 +14,13 @@ import (
 
 const URL = "http://localhost:7000"
 
+var client http.Client
 
 func main() {
 
+	client = http.Client{}
+
 	for {
-
-
 		fmt.Println("Please simply pick one of these 5 options")
 		fmt.Println()
 		fmt.Println("Press 1 to add a new course")
@@ -63,72 +65,55 @@ func main() {
 			fmt.Println(" ")
 			getAllTeachers()
 
-	case "6":
-		fmt.Println("Add a teacher or a student by typing the course id, STUDENT or TEACHER for type and the id of the type (teacherid or studentid)")
-		var courseid, enroller, enrollerid string
-		fmt.Scan(&courseid)
-		fmt.Scan(&enroller)
-		fmt.Scan(&enrollerid)
-		addTeacherStudentToCourse(courseid, enroller, enrollerid)
-	}
+		case "6":
+			fmt.Println("Add a teacher or a student by typing the course id, STUDENT or TEACHER for type and the id of the type (teacherid or studentid)")
+			var courseid, enroller, enrollerid string
+			fmt.Scan(&courseid)
+			fmt.Scan(&enroller)
+			fmt.Scan(&enrollerid)
+			addTeacherStudentToCourse(courseid, enroller, enrollerid)
+		}
 	}
 }
 
+func addTeacherStudentToCourse(courseId string, enrollmentType string, id string) {
+	sendRequest(http.MethodPut, "/courses/"+courseId+"/"+id+"/"+enrollmentType, nil)
+}
+
+func deleteCourse(courseID string) {
+	sendRequest(http.MethodDelete, "/courses/"+courseID, nil)
+}
+
+func deleteTeacherStudent(enrollmentType string, id string, courseId string) {
+	sendRequest(http.MethodDelete, "/courses/"+courseId+"/"+id+"/"+enrollmentType, nil)
+}
+
+func changeNameOfCourse(newCourseName string, courseId string) {
+	sendRequest(http.MethodPut, "/courses/"+courseId, bytes.NewReader([]byte("{title:"+newCourseName+"}")))
+}
+
+func sendRequest(method string, url string, data io.Reader) {
+	req, err := http.NewRequest(method, URL+url, data)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	client.Do(req)
+}
 
 func addCourse(input string) {
-
 	_, err := http.PostForm(URL+"/courses", url.Values{"name": {input}})
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
 
-func addTeacherStudentToCourse(courseId string, enrollmentType string, id string){
+func getAllTeachers() {
 
-	_, err := http.NewRequest("PUT", URL + "/courses/" + courseId + "/"+ enrollmentType + "/" + id, nil)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-}
-
-func deleteCourse(courseID string) {
-	_, err := http.NewRequest("DELETE", URL+"/courses/"+courseID, nil)
-
-	if err != nil {
-		fmt.Println("Error occured")
-	}
-
-}
-
-func deleteTeacherStudent(enrollmentType string, id string, courseId string) {
-	_, err := http.NewRequest("DELETE", URL+"/courses/"+ courseId+"/"+enrollmentType+ "/" + id, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func changeNameOfCourse(newCourseName string, courseId string) {
-
-
-
-	jsonStr := "{title:" + newCourseName + "}"
-
-
-	_, err := http.NewRequest("PUT", URL+"/courses/" + courseId, bytes.NewReader([]byte(jsonStr)))
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-}
-
-func getAllTeachers(){
-
-		resp, err := http.Get(URL + "/courses")
+	resp, err := http.Get(URL + "/courses")
 
 	if err != nil {
 		log.Fatalln(err)
@@ -138,7 +123,6 @@ func getAllTeachers(){
 	var result map[string]interface{}
 	body, err := ioutil.ReadAll(resp.Body)
 
-	// Unmarshal or Decode the JSON to the interface.
 	json.Unmarshal([]byte(body), &result)
 
 	if err != nil {
@@ -146,8 +130,5 @@ func getAllTeachers(){
 		return
 	}
 
-	sb := string(body)
-	log.Println(sb)
+	log.Println(string(body))
 }
-
-
